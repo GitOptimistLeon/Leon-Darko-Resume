58f30c1f2fd63126a9608b485568c68b6b255630
GitOptimistLeon <work.leondarko@gmail.com>
2020-11-06 17:28:08 +0000

    Reset

--- js/myFullScreenNav.js
+++
@@ -1,9 +0,0 @@
-// Fullscreen Navigation script
-    function openNav() {
-      document.getElementById("myNav").style.height = "70%";
-    }
-
-    function closeNav() {
-      document.getElementById("myNav").style.height = "0%";
-    }
-// Fullscreen Navigation script
--- js/mySnackBar.js
+++
@@ -1,7 +0,0 @@
-function snackBarFunction() {
-  var x = document.getElementById("snackbar");
-  x.className = "show";
-  setTimeout(function() {
-    x.className = x.className.replace("show", "");
-  }, 3000);
-}
